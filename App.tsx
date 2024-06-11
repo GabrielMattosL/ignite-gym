0ccafd90c8e565df26b1ce273e037f1a448e3869
Gabriel TJRO <gabriel.mattos@tjro.jus.br>
2024-06-11 11:41:00 -0400

    adicionado a status bar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024'}}>
       {fontsLoaded ? <Text style={{ fontFamily: 'Roboto_700Bold' }}>Hello world!</Text> : <View/>}
+      <StatusBar
+        barStyle= 'light-content'
+        backgroundColor='transparent'
+        translucent={true}
+      />
     </View>
   );
 }
